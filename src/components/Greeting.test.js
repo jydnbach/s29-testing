56894bf664b2ff4af0e19242cb41f4c2be4391bb
jydnbach <jaydenkim95@gmail.com>
2024-07-04 10:45:50 +0900

    group greeting component for testing

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,8 +1,10 @@
 import { screen, render } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders hello world', () => {
-  render(<Greeting />);
-  const helowWorldElement = screen.getByText('hello world', { exact: false });
-  expect(helowWorldElement).toBeInTheDocument();
+describe('Greeting component', () => {
+  test('renders hello world', () => {
+    render(<Greeting />);
+    const helowWorldElement = screen.getByText('hello world', { exact: false });
+    expect(helowWorldElement).toBeInTheDocument();
+  });
 });
