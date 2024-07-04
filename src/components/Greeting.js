2438ef35034a38262b6a71571277dd9eb3ade667
jydnbach <jaydenkim95@gmail.com>
2024-07-04 14:46:42 +0900

    testing 3 different text renders

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+
+      <button onClick={changeTextHandler}>Change text</button>
     </div>
   );
 };
 export default Greeting;
+
+// changedText: test all possible scenarios
