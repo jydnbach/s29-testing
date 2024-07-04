1aae9967c19f2501b0b0657ed7852c7515b4e34a
jydnbach <jaydenkim95@gmail.com>
2024-07-04 15:35:16 +0900

    testing data fetching with mockResolvedValueOnce

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole('listitem'); //find returns promise + 3rd arg: timeout 1s
